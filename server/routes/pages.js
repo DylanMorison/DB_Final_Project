35f07a324f56a7021fbcc13b29b659b6b0ae7f1a
Dylan Morison <dylan@g.morison.org>
2020-10-28 13:08:53 -0700

    installed concurrently plus http middleware

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
 	res.render('index.hbs');
 });
 
-router.get('/register', (req, res) => {
+router.get('/auth/register', (req, res) => {
 	res.render('register.hbs');
 });
 
