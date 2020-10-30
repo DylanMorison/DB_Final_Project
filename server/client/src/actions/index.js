ea465e683cb708d52a79d51379d103948ca5ad0e
Dylan Edwards <dylanedwards290@gmail.com>
2020-10-29 21:15:39 -0700

    added testSql function in sign up

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,15 +1,11 @@
 import axios from "axios";
-import { FETCH_USER, FETCH_USERS } from "./types";
+import { CREATE_USER, FETCH_USERS } from "./types";
 
-export const createUser = () => async dispatch => {
-	const testUser = {
-		email: "test@test.com",
-		password: "password",
-		username: "username"
-	};
-	const res = await axios.post("/auth/register", testUser);
+export const createUser = (userData) => async dispatch => {
+
+	const res = await axios.post("/auth/register", userData);
 	console.log(res);
-	dispatch({ type: FETCH_USER, payload: res.data });
+	dispatch({ type: CREATE_USER, payload: res.data });
 };
 
 export const getAllUsers = () => async dispatch => {
