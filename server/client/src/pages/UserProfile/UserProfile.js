e9b0b879d72daf1bc5a9477894089615e6fc893b
Dylan Morison <dylan@g.morison.org>
2020-11-09 21:17:15 -0800

    finished full stack implementation of register user, and login user

--- server/client/src/pages/UserProfile/UserProfile.js
+++ server/client/src/pages/UserProfile/UserProfile.js
@@ -14,81 +14,77 @@ import { connect } from "react-redux";
 import { useSelector } from "react-redux";
 
 const Spacing = styled.div`
-  padding: 15px;
+	padding: 15px;
 `;
 
 const Container = styled.div`
-  position: relative;
-  margin: 0 auto;
-  margin-top: ${(p) => (p.marginTop ? p.theme.spacing[p.marginTop] : 0)};
-  width: 100%;
-  max-width: ${(p) => p.maxWidth && p.theme.screen[p.maxWidth]};
-  padding: ${(p) =>
-    p.padding ? `0 ${p.theme.spacing[p.padding]}` : `0 ${p.theme.spacing.sm}`};
-  z-index: ${(p) => p.zIndex && p.theme.zIndex[p.zIndex]};
-  background-color: inherit;
-  border-radius: ${(p) => p.radius && p.theme.radius[p.radius]};
+	position: relative;
+	margin: 0 auto;
+	margin-top: ${(p) => (p.marginTop ? p.theme.spacing[p.marginTop] : 0)};
+	width: 100%;
+	max-width: ${(p) => p.maxWidth && p.theme.screen[p.maxWidth]};
+	padding: ${(p) =>
+		p.padding ? `0 ${p.theme.spacing[p.padding]}` : `0 ${p.theme.spacing.sm}`};
+	z-index: ${(p) => p.zIndex && p.theme.zIndex[p.zIndex]};
+	background-color: inherit;
+	border-radius: ${(p) => p.radius && p.theme.radius[p.radius]};
 
-  @media (max-width: 1000px) {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+	@media (max-width: 1000px) {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 `;
 
 const Root = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: red;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	width: 100vw;
+	background-color: red;
 `;
 
 const UserProfile = (props) => {
-  const { auth } = props.location.state; // pulls out the auth from the state passed by <Link/> in react router
-  const thisUser = useSelector(state => state.users.usersByUid[auth])
-  const thisUserPosts = useSelector(state => state.posts.postsByUids, postsByUids => postsByUids.filter(post => post.authorUid == auth))
-  let userPostLength = Object.keys(thisUserPosts).length;
-  let Posts = [];
-  for (const post in thisUserPosts) {
-      Post.push(post)
-  }
+	const { auth } = props.location.state; // pulls out the auth from the state passed by <Link/> in react router
+	const thisUser = useSelector((state) => state.users.usersByUid[auth]);
+	const thisUserPosts = useSelector(
+		(state) => state.posts.postsByUids,
+		(postsByUids) => postsByUids.filter((post) => post.authorUid == auth)
+	);
+	let userPostLength = Object.keys(thisUserPosts).length;
+	let Posts = [];
+	for (const post in thisUserPosts) {
+		Post.push(post);
+	}
 
-
-
-  return (
-    <Container maxWidth="sm">
-      <Spacing />
-      <UserHeader
-        user={thisUser.email}
-        userData={thisUser}
-        followers={thisUser.followers}
-        following={thisUser.following}
-        posts={1}
-      />
-      <Spacing />
-      {userPostLength > 0 ? (
-        thisUserPosts.map((postUid) => (
-          <Post
-          postUid={postUid}
-          />
-        ))
-      ) : (
-        <div> no posts by this user </div>
-      )}
-    </Container>
-  );
+	return (
+		<Container maxWidth="sm">
+			<Spacing />
+			<UserHeader
+				user={thisUser.email}
+				userData={thisUser}
+				followers={thisUser.followers}
+				following={thisUser.following}
+				posts={1}
+			/>
+			<Spacing />
+			{userPostLength > 0 ? (
+				thisUserPosts.map((postUid) => <Post postUid={postUid} />)
+			) : (
+				<div> no posts by this user </div>
+			)}
+		</Container>
+	);
 };
 
 function mapStatetoProps(state) {
-  return {
-    posts: state.posts,
-    users: state.users,
-  };
+	return {
+		posts: state.posts,
+		users: state.users
+	};
 }
 
 export default connect(mapStatetoProps)(UserProfile);
-
