646b0a016aff77b8e90212b25f8881c2a510f6fb
Dylan Edwards <dylanedwards290@gmail.com>
2020-11-21 21:32:22 -0800

    allowed users to follow and unfollow from the user profile page and added links to user profile from posts

--- server/client/src/pages/UserProfile/UserSection.js
+++ server/client/src/pages/UserProfile/UserSection.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 import Avatar from "components/Avatar";
 import defaultPic from "../../img/default-pic.png";
 import GearIcon from "../../components/icons/GearIcon";
-import { connect } from "react-redux";
 import { logOut } from "../../actions";
 import { Button } from "react-bootstrap";
+import AddButton from "../../pages/People/AddButton"
+import { connect, useSelector } from "react-redux";
+import { followUser, unfollowUser} from "../../actions"
 
 const UserWrapper = styled.div`
   display: flex;
@@ -59,14 +61,35 @@ const AvatarWrapper = styled.div`
   cursor: pointer;
 `;
 
+const Spacing = styled.div`
+  padding-bottom: 10px;
+`;
+
+
 const UserSection = (props) => {
-  const [modalShow, setModalShow] = useState(false);
+  console.log(props.userData)
+  const loggedInUser = useSelector((state) => state.users.usersByUid[props.auth.userUid]);
+  const thisUser = useSelector((state) => state.users.usersByUid[props.userData.userUid]);
+  const [following, setFollowing] = useState(loggedInUser.following.includes(thisUser.userUid));
+
 
   const handleLogOut = (params) => {
     if (window.confirm("are you sure you want to log out?")) {
       props.logOut();
     }
   };
+
+  const handleFollowing = () => {
+    if (following) {
+      props.unfollowUser(props.auth.userUid, thisUser.userUid)
+      setFollowing(!following);
+    }
+    else{
+      props.followUser(props.auth.userUid, thisUser.userUid)
+      setFollowing(!following);
+    }
+  };
+
 
   return (
     <>
@@ -80,6 +103,8 @@ const UserSection = (props) => {
         <UserNameWrapper>
           <UserTitle>Creator:</UserTitle>
           <UserName>{props.userData.username}</UserName>
+          <Spacing/>
+          <AddButton handleFollowing={handleFollowing} following={following} />
         </UserNameWrapper>
       </UserWrapper>
     </>
@@ -89,7 +114,8 @@ const UserSection = (props) => {
 function mapStatetoProps(state) {
   return {
     auth: state.auth,
+    users: state.users
   };
 }
 
-export default connect(mapStatetoProps, { logOut })(UserSection);
+export default connect(mapStatetoProps, {  followUser, unfollowUser })(UserSection);
